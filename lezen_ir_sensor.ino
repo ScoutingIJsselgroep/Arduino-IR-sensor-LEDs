63cfedc54424c0ffe697900d8cf9073a6856e7e0
Dennis Veeneman <dennis_veeneman@hotmail.com>
2018-12-06 21:05:54 +0100

    init

---
+++ lezen_ir_sensor.ino
@@ -0,0 +1,145 @@
+#include <IRremote.h>
+
+//set D2 as input signal pin
+int input_pin = 2;
+
+// 6 PWM port: D3, D5, D6, D9, D10, D11
+int r_pin = 9;
+int g_pin = 10;
+int b_pin = 5;
+int y_pin = 6;
+
+// status voor het programma
+int state;
+
+// duur voor faden
+int duration = 333;
+
+IRrecv irrecv(input_pin);
+decode_results signals;
+
+// signaal van de afstandsbediening dat we gaan gebruiken kan hier in
+unsigned long firstValue;
+
+void setup() {
+  Serial.begin(4800);
+  
+  irrecv.enableIRIn();
+
+  // zet de LED pins als output, en de LEDs uit
+  pinMode(r_pin, OUTPUT);
+  pinMode(g_pin, OUTPUT);
+  pinMode(b_pin, OUTPUT);
+  pinMode(y_pin, OUTPUT);
+  digitalWrite(r_pin, 0);
+  digitalWrite(g_pin, 0);
+  digitalWrite(b_pin, 0);
+  digitalWrite(y_pin, 0);
+
+  state = -1;
+}
+
+
+void loop() {
+  if(state == -1) {
+    if (irrecv.decode(&signals)) {
+      Serial.print(signals.value, HEX);
+      Serial.println(" <- Assigned");
+
+      // eerst herkende waarde gaan we gebruiken 
+      firstValue = signals.value;
+
+      // volgende signaal ontvangen
+      irrecv.resume();
+
+      state = 0;
+    }
+  } else {
+    if (irrecv.decode(&signals)) {
+      if(firstValue != signals.value) {
+        Serial.print(signals.value, HEX);
+        Serial.println(" <- Ignored");
+        
+        // volgende signaal ontvangen
+        irrecv.resume();
+      } else {
+        Serial.print(signals.value, HEX);
+        Serial.println(" <- Used");
+        
+        // volgende signaal ontvangen
+        irrecv.resume();
+
+        // naar de volgende stand
+        state++;
+        
+        // alle LEDs laten knipperen
+        digitalWrite(r_pin, HIGH);
+        digitalWrite(g_pin, HIGH);
+        digitalWrite(b_pin, HIGH);
+        digitalWrite(y_pin, HIGH);
+        delay (300);
+        digitalWrite(r_pin, 0);
+        digitalWrite(g_pin, 0);
+        digitalWrite(b_pin, 0);
+        digitalWrite(y_pin, 0);
+        delay (300);
+        digitalWrite(r_pin, HIGH);
+        digitalWrite(g_pin, HIGH);
+        digitalWrite(b_pin, HIGH);
+        digitalWrite(y_pin, HIGH);
+        delay (300);
+        digitalWrite(r_pin, 0);
+        digitalWrite(g_pin, 0);
+        digitalWrite(b_pin, 0);
+        digitalWrite(y_pin, 0);
+      
+        if (state == 1) {
+          digitalWrite(r_pin, HIGH);
+        } else if (state == 2) {
+          digitalWrite(g_pin, HIGH);
+        } else if (state == 3) {
+          digitalWrite(b_pin, HIGH);
+        } else if (state == 4) {
+          digitalWrite(y_pin, HIGH);
+        } else if (state == 5) {
+          digitalWrite(r_pin, HIGH);
+          digitalWrite(g_pin, HIGH);
+          digitalWrite(b_pin, HIGH);
+          digitalWrite(y_pin, HIGH);
+        } else {
+          // 0 is faden, dit gebeurt hieronder
+          state = 0;
+        }
+      }
+    }
+
+    // faden
+    if(state == 0) {
+      long val = (millis() / 25) % duration;
+      int part = duration / 4;
+      long fase = ((val % part) * 255) / part;
+      
+      if(val < part) {
+        analogWrite(r_pin, fase);
+        analogWrite(g_pin, 0);
+        analogWrite(b_pin, 0);
+        analogWrite(y_pin, 255 - fase);
+      } else if(val < part * 2) {
+        analogWrite(r_pin, 255 - fase);
+        analogWrite(g_pin, fase);
+        analogWrite(b_pin, 0);
+        analogWrite(y_pin, 0);
+      } else if(val < part * 3) {
+        analogWrite(r_pin, 0);
+        analogWrite(g_pin, 255 - fase);
+        analogWrite(b_pin, fase);
+        analogWrite(y_pin, 0);
+      } else {
+        analogWrite(r_pin, 0);
+        analogWrite(g_pin, 0);
+        analogWrite(b_pin, 255 - fase);
+        analogWrite(y_pin, fase);
+      }
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Met de afstandsbediening LEDs bedienen
+
+## Solderen
+
+Voor dit kun je een [Arduino Pro mini ATMEGA328P 3.3V 8M](https://www.dennisdeal.com/products/10pcs-pro-mini-atmega328p-3-3v-8m-interactive-media-upgrade-version-for-arduino) gebruiken.
+
+De LEDs kunnen met een weerstand van 100 Ohm aan de PWM pins worden gemaakt.
+Rood op 9, groen op 10, blauw op 5, en geel op 6.
+Maar je kan natuurlijk ook andere kleuren gebruiken
+
+IR ontvanger CHQ1838 is aangesloten op pin 2
+
+
+## Het programma op een Arduino Pro mini zetten
+
+De Pro mini heeft zelf geen USB aansluiting om het programma er op te kunnen zetten.
+Dit kan met een Arduino Uno, haal de microcontroller er voorzichtig uit zodat je niet deze schrijft maar de mini die je er op aan sluit.
+
+
+## Het programma zelf
+
+Het eerste deel (state -1) is om een signaal van een afstandsbediening op te vangen, deze word onthouden.
+Het volgende deel is dat de lampjes gaan branden. Er zijn meerdere opties waartussen gewisseld kan worden door dezelfde knop op de afstandsbediening te gebruiken als aan het begin.
+
++ Faden, de lampjes gaan om en om feller en minder fel branden
++ Alleen rood aan
++ Alleen groen aan
++ Alleen blauw aan
++ Alleen geel aan
++ Allemaal aan
+
